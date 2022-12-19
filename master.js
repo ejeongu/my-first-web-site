62e07f069dfe73c2eaa7a51b9db4131a57182a54
ejeongu <114378707+ejeongu@users.noreply.github.com>
2022-12-19 23:49:40 +0900

    Add files via upload

--- master.js
+++ master.js
@@ -1,65 +1,29 @@
 var Links = {
-  setColor : function(color) {
-    var alist = document.querySelectorAll('a');
-     var i = 0;
-     while (i < alist.length) {
-       alist[i].style.color = color;
-       i = i + 1;
-     }
+  setColor : function(color){
+    $('a').css('color', color);
   }
 };
+
 var Body = {
-  setColor : function (color){
-    document.querySelector('body').style.color = color;
+  setColor : function(color){
+    $('body').css('color', color);
   },
-  setBGcolor : function (color){
-    document.querySelector('body').style.backgroundColor = color;
-    }
+  setBackgroundColor : function(color){
+    $('body').css('backgroundColor', color);
   }
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-//function linkssetColor(color) {
-   //var alist = document.querySelectorAll('a');
-    //var i = 0;
-  //  while (i < alist.length) {
-    //  alist[i].style.color = color;
-    //  i = i + 1;
-    //}
-//  }
+};
 
-  //function  bodysetcolor (color){
-  //  document.querySelector('body').style.color = color;
-//  }
-
-//function bodysetbgcolor(color){
-  //document.querySelector('body').style.color = color;
-//}
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 function nightDayHandler(self){
-  //var target = document.querySelector('body');
-
   if(self.value == 'night'){
-  Body.setBGcolor('black');
-    //bodysetbgcolor('black');
-  //  target.style.backgroundColor = 'black';
-  Body.setColor('white');
-  //bodysetcolor('white');
-  //  target.style.color = 'white';
-  Links.setColor('powderblue');
-  //  linkssetcolor('powderblue');
-   self.value = 'day';
-
-//setcolor('powderblue')
-
-  }
-  else {
-    Body.setBGcolor('white');
-    //target.style.backgroundColor = 'white';
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+    Links.setColor('powderblue')
+  } else {
+    Body.setBackgroundColor('white')
     Body.setColor('black');
-    //target.style.color = 'black';
-    Links.setColor('blue');
     self.value = 'night';
-    //self.value = 'night';
+    Links.setColor('blue');
 
-    //setColor('blue')
   }
 }
